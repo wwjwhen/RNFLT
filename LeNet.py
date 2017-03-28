6e14007a1959ce4b03771d9244a5ead4987235aa
unknown <>
2017-03-28 10:03:29 +0800

    new version

--- LeNet.py
+++ LeNet.py
@@ -1,45 +1,18 @@
-"""
-This tutorial introduces the LeNet5 neural network architecture
-using Theano.  LeNet5 is a convolutional neural network, good for
-classifying images. This tutorial shows how to build the architecture,
-and comes with all the hyper-parameters you need to reproduce the
-paper's MNIST results.
-
-
-This implementation simplifies the model in the following ways:
-
- - LeNetConvPool doesn't implement location-specific gain and bias parameters
- - LeNetConvPool doesn't implement pooling by average, it implements pooling
-   by max.
- - Digit classification is implemented with a logistic regression rather than
-   an RBF network
- - LeNet5 was not fully-connected convolutions at second layer
-
-References:
- - Y. LeCun, L. Bottou, Y. Bengio and P. Haffner:
-   Gradient-Based Learning Applied to Document
-   Recognition, Proceedings of the IEEE, 86(11):2278-2324, November 1998.
-   http://yann.lecun.com/exdb/publis/pdf/lecun-98.pdf
-
-"""
-
 from __future__ import print_function
-
 import os
 import sys
 import timeit
-
 import numpy
 from numpy import genfromtxt
 import theano
 import theano.tensor as T
 from theano.tensor.signal import pool
 from theano.tensor.nnet import conv2d
-
 from logistic_sgd import LogisticRegression
 from format import load_data
 from mlp import HiddenLayer
 from sklearn.metrics import roc_curve, auc
+
 
 class LeNetConvPoolLayer(object):
     """Pool Layer of a convolutional network """
@@ -119,7 +92,7 @@ class LeNetConvPoolLayer(object):
 
 
 
-def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
+def evaluate_lenet5(learning_rate=0.01, n_epochs=200,
                     dataset='mnist.pkl.gz',
                     nkerns=[20, 20, 20, 20, 50], batch_size=100):
     """ Demonstrates lenet on MNIST dataset
@@ -222,19 +195,25 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
     layer45 = LeNetConvPoolLayer(
         rng,
         input=layer4.output,
-        image_shape=(batch_size, 20, 1, 21),
-        filter_shape=(50, 20, 1, 4),
+        image_shape=(batch_size, 50, 1, 21),
+        filter_shape=(50, 50, 1, 4),
         poolsize=(1, 2))
-    
-    
+    '''
+    layer452 = LeNetConvPoolLayer(
+        rng,
+        input=layer451.output,
+        image_shape=(batch_size, 50, 1, 9),
+        filter_shape=(50, 50, 1, 4),
+        poolsize=(1, 2))
+    '''
     # here we expect to reduce two conv-pool layers
-    layer5_input = layer45.output.flatten(2)
+    layer5_input = layer4.output.flatten(2)
     
     
     layer5 = HiddenLayer(
         rng,
         input=layer5_input,
-        n_in=nkerns[4] * 1 * 9,
+        n_in=nkerns[4] * 1 * 21,
         n_out=50,
         activation=T.tanh)
     # for dropout
@@ -243,6 +222,20 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
     mask = srng.binomial(n=1, p=0.5, size=layer5.output.shape)
    
     layer6 = LogisticRegression(input=layer5.output, n_in=50, n_out=2)
+    #layer6a = LogisticRegression(input=layer5.output * 0.5, n_in=50, n_out=2)
+    
+    '''
+    f = file('w1.npy', 'wb')
+    numpy.save(f, layer0.W.get_value())
+    numpy.save(f, layer1.W.get_value())
+    numpy.save(f, layer2.W.get_value())
+    numpy.save(f, layer3.W.get_value())
+    numpy.save(f, layer4.W.get_value())
+    numpy.save(f, layer5.W.get_value())
+    numpy.save(f, layer6.W.get_value())
+    print(layer0.W.get_value())
+    f.close()
+    '''
     
     f = file('w4.npy', 'rb')
     layer0.W.set_value(numpy.load(f))
@@ -252,7 +245,7 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
     layer4.W.set_value(numpy.load(f))
     layer5.W.set_value(numpy.load(f))
     layer6.W.set_value(numpy.load(f))
-
+    
     # the cost we minimize during training is the NLL of the model
     cost = layer6.negative_log_likelihood(y)
 
@@ -293,13 +286,23 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
 
     train_model = theano.function(
         [index],
-        [cost, grads[0]],
+        [cost, layer6.errors(y)],
         updates=updates,
         givens={
             x: train_set_x[index * batch_size: (index + 1) * batch_size],
             y: train_set_y[index * batch_size: (index + 1) * batch_size]
         }
     )
+
+    train_loss = theano.function(
+        [index],
+        layer6.errors(y),
+        givens={
+            x: train_set_x[index * batch_size: (index + 1) * batch_size],
+            y: train_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+
     # end-snippet-1
 
     ###############
@@ -307,7 +310,7 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
     ###############
     print('... training')
     # early-stopping parameters
-    patience = 15000  # look as this many examples regardless
+    patience = 25000  # look as this many examples regardless
     patience_increase = 2  # wait this much longer when a new best is
     # found
     improvement_threshold = 0.995  # a relative improvement of this much is
@@ -323,17 +326,12 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
     best_paras = []
     test_score = 0.
     start_time = timeit.default_timer()
-    '''
-    lab = theano.function(
-	inputs=[layer0.input],
-	outputs=layer6.y_pred)
-    '''
+
     lab_valid = theano.function(
         [index],
         layer6.y_pred,
         givens={
             x: valid_set_x[index * batch_size: (index + 1) * batch_size]
-            #y: valid_set_y[index * batch_size: (index + 1) * batch_size]
         }
     )
 
@@ -342,7 +340,6 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
         layer6.y_pred,
         givens={
             x: test_set_x[index * batch_size: (index + 1) * batch_size]
-            #y: test_set_y[index * batch_size: (index + 1) * batch_size]
         }
     )
 
@@ -351,7 +348,6 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
         layer6.p_y_given_x,
         givens={
             x: test_set_x[index * batch_size: (index + 1) * batch_size]
-            #y: test_set_y[index * batch_size: (index + 1) * batch_size]
         }
     )
 
@@ -391,7 +387,7 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
         }
     )
 
-
+    ftv = file('train_valid5.txt', 'wb')
     epoch = 0
     done_looping = False
     print('the patience is ', patience)
@@ -404,19 +400,28 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
             if iter % 100 == 0:
                 print('training @ iter = ', iter)
             costij, gradient = train_model(minibatch_index)
-            print("the cost is " + str(costij))
-            print(gradient[0])
+            #print("the cost is " + str(costij))
+            #print(gradient[0])
             if (iter + 1) % validation_frequency == 0:
 
                 # compute zero-one loss on validation set
-                validation_losses = [validate_model(i) for i
-                                     in range(n_valid_batches)]
+                #layer6a.W.set_value(layer6.W.get_value())
+                #layer6a.b.set_value(layer6.b.get_value())
+                validation_losses = [validate_model(i) for i in range(n_valid_batches)]
                 this_validation_loss = numpy.mean(validation_losses)
-                print('epoch %i, minibatch %i/%i, validation error %f %%' %
+
+                train_losses = [train_loss(i) for i in range(n_train_batches)]
+                this_train_loss = numpy.mean(train_losses)
+                ftv.write(str(this_train_loss) + ' ' + str(this_validation_loss) + "\n")
+                print('epoch %i, minibatch %i/%i, validation error %f %%, train error %f %%' %
                       (epoch, minibatch_index + 1, n_train_batches,
-                       this_validation_loss * 100.))
+                       this_validation_loss * 100., this_train_loss * 100.))
+                '''
+		print(lab(valid_set_x.get_value()[0:batch_size].reshape((batch_size, 1, 1, 768))))
+                '''
                 print(lab_valid(0))
                 print(type(valid_set_y))
+	        #print(valid_set_y.get_value()[0:batch_size])
                 # if we got the best validation score until now
                 if this_validation_loss < best_validation_loss:
                     # improve patience if loss improvement is good enough
@@ -428,7 +433,7 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
                     # save best validation score and iteration number
                     best_validation_loss = this_validation_loss
                     best_iter = iter
-		            best_paras = [layer6.params[0].get_value(), layer6.params[1].get_value(), layer5.params[0].get_value(), layer5.params[1].get_value(), layer4.params[0].get_value(), layer4.params[1].get_value(), layer3.params[0].get_value(), layer3.params[1].get_value(), layer2.params[0].get_value(), layer2.params[1].get_value(), layer1.params[0].get_value(), layer1.params[1].get_value(), layer0.params[0].get_value(), layer0.params[1].get_value()]
+		    best_paras = [layer6.params[0].get_value(), layer6.params[1].get_value(), layer5.params[0].get_value(), layer5.params[1].get_value(), layer4.params[0].get_value(), layer4.params[1].get_value(), layer3.params[0].get_value(), layer3.params[1].get_value(), layer2.params[0].get_value(), layer2.params[1].get_value(), layer1.params[0].get_value(), layer1.params[1].get_value(), layer0.params[0].get_value(), layer0.params[1].get_value(), layer45.params[0].get_value(), layer45.params[1].get_value()]
                     #print(layer6.params[0].get_value()) 
                     # test it on the test set
                     #layer6a.W.set_value(layer6.W.get_value())
@@ -450,6 +455,9 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
             print('epoch and iter are: ' + str(epoch) + ' ' + str(iter))
 
     
+    #print(layer6.params[0].get_value())
+    #print(best_paras[0])
+    
     layer6.params[0].set_value(best_paras[0])
     layer6.params[1].set_value(best_paras[1])
     layer5.params[0].set_value(best_paras[2])
@@ -464,16 +472,31 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
     layer1.params[1].set_value(best_paras[11])
     layer0.params[0].set_value(best_paras[12])
     layer0.params[1].set_value(best_paras[13])
+    layer45.params[0].set_value(best_paras[14])
+    layer45.params[1].set_value(best_paras[15])
     
     f = file("traindata.npy", "rb")
     train_datas = numpy.load(f)
     train_labels = numpy.load(f)
     test_datas = numpy.load(f)[1000:5400]
     test_labels = numpy.load(f)[1000:5400]
+    #test_datas = train_datas
+    #test_labels = train_labels
     datas = test_datas[:100]
     print(datas.shape)
     
+
+    def smooth(grad):
+        grad = grad_x(0, 0)
+        print(grad)
+        print(grad.shape)
+        for i in range(0, 768 - 3, 1):
+            grad[:, i] = (grad[:, i] + grad[:, i + 1] + grad[:, i + 2] + grad[:, i + 3]) / 4
+        print(grad)
+        return grad
+    
     X = theano.shared(value=datas, borrow=True)
+    
     updates_x = [(X, X - 0.001 * gradX)]
 
     restore_x = theano.function(
@@ -485,22 +508,61 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
     )
     
     fr = file("revised.txt", "wb")
+    #grad_value = theano.function(inputs=[num, x], outputs=[gradX])
 
     for ind in range(n_test_batches):
         X.set_value(test_datas[ind * batch_size: (ind + 1) * batch_size])
-        iter = 10
+        iter = 1000
         while iter > 0:
             p = restore_x(X.get_value())
             if iter % 100==0 and ind ==0:
                 print(p)
+                #print(grad_x(0, 0)[0])
             iter -= 1
         for i in range(batch_size):
             for x in (X.get_value()[i]):
                 fr.write(str(x) + " ")
             fr.write('\n')
         print(X.get_value().shape)
+        #break
     fr.close()
 
+    # get the grad of one batch_size samples
+    
+    f = open("grad.txt", "wb")
+    for x in range(batch_size):
+        for y in grad_x(0, 0)[x]:
+            f.write(str(y) + " ")
+        f.write("\n")
+   
+    '''
+    f1 = open("feat1.txt", "w")
+    f2 = open("feat2.txt", "w")
+    f3 = open("feat3.txt", "w")
+    f4 = open("feat4.txt", "w")
+
+    for ind in range(10):
+        for x in range(batch_size):
+            for y in get_layer1_out(ind)[0][x][0][0]:
+                f1.write(str(y) + " ")
+            f1.write("\n")
+        for x in range(batch_size):
+            for y in get_layer2_out(ind)[0][x][0][0]:
+                f2.write(str(y) + " ")
+            f2.write("\n")
+        for x in range(batch_size):
+            for y in get_layer3_out(ind)[0][x][0][0]:
+                f3.write(str(y) + " ")
+            f3.write("\n")
+        for x in range(batch_size):
+            for y in get_layer4_out(ind)[0][x][0][0]:
+                f4.write(str(y) + " ")
+            f4.write("\n")
+    '''
+
+    #print(grad_x(1, 0)[0][1])
+    #print(layer6.params[0].get_value())
+    #print(lab(test_set_x.get_value()[0:600].reshape((600, 1, 1, 768))))
     test_labels = [lab_test(i) for i in range(6)]
     print(numpy.array(test_labels).flatten())
     fp = open('test_result.txt', 'wb')
@@ -508,15 +570,23 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
     for x in test_labels:
         fp.write(str(x) + ' ')
     print(test_set_y.get_value()[0:600])
+    #prob = theano.function(inputs=[layer0.input], outputs=[layer6.p_y_given_x])
     print(test_set_y.get_value().shape)
+    #pro = prob(test_set_x.get_value().reshape((5446, 1, 1, 768)))
     print("prob in index 0 is")
     print(prob_test(0)[:, 1])
     pro = [prob_test(i) for i in range(n_test_batches)]
+    #print(numpy.array(pro[0]))
     pro = numpy.array(pro)
-    pro = pro.reshape(3400, 2)
+    pro = pro.reshape(4400, 2)
     print(pro.shape)
 
-    fpr, tpr, thres = roc_curve(test_set_y.get_value()[:3400], pro[:, 1], pos_label=1)
+    fpr, tpr, thres = roc_curve(test_set_y.get_value()[:4400], pro[:, 1], pos_label=1)
+    print("mean and std are:")
+    print(numpy.mean(pro[:, 0]))
+    print(numpy.var(pro[:, 0]))
+    print(numpy.mean(pro[:, 1]))
+    print(numpy.var(pro[:, 1]))
     fp = open('fpr_tpr.txt', 'w')
     for x in fpr:
         fp.write(str(x) + " ")
@@ -536,6 +606,6 @@ def evaluate_lenet5(learning_rate=0.01, n_epochs=1000,
     print(('The code for file ' +
            os.path.split(__file__)[1] +
            ' ran for %.2fm' % ((end_time - start_time) / 60.)), file=sys.stderr)
-
+    print(gradX)
 if __name__ == '__main__':
     evaluate_lenet5()
